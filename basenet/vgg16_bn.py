1a79bbbf0f7a634e1458bd5d3c07880dd04ee601
inouetaka <webroot63@gmail.com>
2019-08-09 14:30:20 +0900

    タイポ削除

--- basenet/vgg16_bn.py
+++ basenet/vgg16_bn.py
@@ -15,6 +15,7 @@ import torch.nn.init as init
 from torchvision import models
 from torchvision.models.vgg import model_urls
 
+
 def init_weights(modules):
     for m in modules:
         if isinstance(m, nn.Conv2d):
@@ -27,7 +28,8 @@ def init_weights(modules):
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
-                
+
+
 class vgg16_bn(torch.nn.Module):
     def __init__(self, pertrained=True, freeze=True):
         super(vgg16_bn, self).__init__()
