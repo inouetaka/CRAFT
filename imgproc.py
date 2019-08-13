4f263a8e66af7f5469b078afdbfb00d90758998e
inouetaka <webroot63@gmail.com>
2019-08-13 12:41:34 +0900

    fix タイポ

--- imgproc.py
+++ imgproc.py
@@ -25,7 +25,7 @@ def normalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0
     img = in_img.copy().astype(np.float32)
     
     img -= np.array([mean[0] * 255.0, mean[1] * 255.0, mean[2] * 255.0], dtype=np.float32)
-    img /= np.array([variance[0] * 255.0, variance[1] * 255.0, varinace[2] * 255.0], dtype=np.float32)
+    img /= np.array([variance[0] * 255.0, variance[1] * 255.0, variance[2] * 255.0], dtype=np.float32)
     return img
 
 def denormalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)):
